fe27ef7e9b9c0301ad06227cca79009f6407be2b
LilianBittar <lilian.bittar@pm.me>
2025-04-05 15:11:22 +0200

    Implemented lazy mapper.

---
+++ Catalog.Application/Mappers/ProductMapper.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Mappers
+{
+    public static class ProductMapper
+    {
+        private static readonly Lazy<IMapper> _mapper = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.SetMethod.IsAssembly;
+                cfg.AddProfile<ProductMappingProfile>();
+            });
+            var mapper = config.CreateMapper();
+            return mapper;
+        });
+
+        public static IMapper Mapper => _mapper.Value;
+    }
+}
