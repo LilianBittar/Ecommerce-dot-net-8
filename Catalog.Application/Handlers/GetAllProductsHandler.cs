390ab35d7a1d74f0b68c93a4bb031e40c174a9f3
LilianBittar <lilian.bittar@pm.me>
2025-04-06 12:21:46 +0200

    Implementing get all product handler.

---
+++ Catalog.Application/Handlers/GetAllProductsHandler.cs
@@ -0,0 +1,24 @@
+﻿
+using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetAllProductsHandler : IRequestHandler<GetAllProductQuery, IList<ProductResponse>>
+    {
+        private readonly IProductRepository _repository;
+        public GetAllProductsHandler(IProductRepository productRepository) 
+        { 
+            _repository = productRepository;
+        }
+        public async Task<IList<ProductResponse>> Handle(GetAllProductQuery request, CancellationToken cancellationToken)
+        {
+            var productList = await _repository.GetProducts();
+            var productResponseList = ProductMapper.Mapper.Map<IList<ProductResponse>>(productList);
+            return productResponseList;
+        }
+    }
+}
